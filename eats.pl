eaddec109f2cc5238ea6065d50fa960dfcf74c96
mcs203 <102087805+mcs203@users.noreply.github.com>
2023-12-06 20:33:31 -0500

    eats example

---
+++ eats.pl
@@ -0,0 +1,15 @@
+% This knowledge base covers the things that Pac-Man eats. (Data taken from the 1980 arcade game.)
+eats(pac-man, pac-dots).
+eats(pac-man, "power pellets").
+eats(pac-man, inky).
+eats(pac-man, pinky).
+eats(pac-man, blinky).
+eats(pac-man, clyde).
+eats(pac-man, cherry).
+eats(pac-man, strawberry).
+eats(pac-man, orange).
+eats(pac-man, apple).
+eats(pac-man, melon).
+eats(pac-man, galaxian).
+eats(pac-man, bell).
+eats(pac-man, key).
